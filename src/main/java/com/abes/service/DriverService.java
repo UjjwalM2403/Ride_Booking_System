ae232d34b096e89f631cf85a262e2dfc6a4fa255
unknown <ujjwalmittal2403@gmail.com>
2025-05-30 15:26:54 +0530

    Service Package Update-1 by Ujjwal

--- src/main/java/com/abes/service/DriverService.java
+++ src/main/java/com/abes/service/DriverService.java
@@ -1,5 +1,18 @@
 package com.abes.service;
 
-public class DriverService {
+import com.abes.dto.*;
+import com.abes.exception.*;
+import java.util.List;
 
+public interface DriverService {
+    ResponseDto registerDriver(Driver driver, Vehicle vehicle) throws InvalidCredentialsException;
+    Driver getDriverById(Long id) throws DriverNotFoundException;
+    Driver getDriverByEmail(String email) throws DriverNotFoundException;
+    List<Driver> getAvailableDrivers();
+    List<Driver> getAllDrivers();
+    ResponseDto updateDriver(Driver driver) throws DriverNotFoundException;
+    ResponseDto deleteDriver(Long id) throws DriverNotFoundException;
+    ResponseDto updateDriverLocation(Long driverId, Location location) throws DriverNotFoundException;
+    ResponseDto updateDriverAvailability(Long driverId, Boolean isAvailable) throws DriverNotFoundException;
+    List<Driver> findNearbyDrivers(Location location, double radiusKm);
 }
